0f27166b8c2918154e2062a3bfd05f5a017b4340
Abdullahi254 <abdush504@gmail.com>
2023-03-20 23:32:18 +0300

    completed 2nd class

--- script.ts
+++ script.ts
@@ -2,14 +2,24 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const user = await prisma.user.create({
+    const user = await prisma.user.update({
         data:{
-            name:"Safia"
+            age:{
+                multiply:6
+            }
+        },
+        where:{
+            email: "papa@gmail.com"
+        },
+        select:{
+            name:true,
+            age:true
         }
     })
     console.log(user)
 }
 main().catch(e => {
+    console.log("Error |:")
     console.log(e.message)
 }).finally(async () => {
     await prisma.$disconnect()
